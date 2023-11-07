7cbd1aac034a195b1e0cf9fe6a950f0cc90dff30
Tlinton1 <tenishal@hotmail.co.uk>
2023-11-07 19:28:03 +0000

    done

--- session-5/index.js
+++ session-5/index.js
@@ -1,24 +1,45 @@
-//standard way of writing a function 
+//standard way of writing a function
 function getGrade(total) {
-    
-     if (total > 90) {
-        grade = "A+";
-    } else if (total > 80 && total <= 90) {
-        grade = "A";
-    } else if (total > 70 && total <= 80) {
-        grade = "B";
-    } else if (total > 60 && total <= 70) {
-        grade = "C";
-    } else if (total > 50 && total <= 60) {
-        grade = "D";
-    } else if (total > 40 && total <= 50) {
-        grade = "E";
-    } else if (total > 30 && total <= 40) {
-        grade = "F";
-    } else if (total <= 30) {
-        grade = "FAIL !!!";
-    }
-    return grade;
+  if (total > 90) {
+    grade = "A+";
+  } else if (total > 80 && total <= 90) {
+    grade = "A";
+  } else if (total > 70 && total <= 80) {
+    grade = "B";
+  } else if (total > 60 && total <= 70) {
+    grade = "C";
+  } else if (total > 50 && total <= 60) {
+    grade = "D";
+  } else if (total > 40 && total <= 50) {
+    grade = "E";
+  } else if (total > 30 && total <= 40) {
+    grade = "F";
+  } else if (total <= 30) {
+    grade = "FAIL !!!";
+  }
+  return grade;
 }
 
-console.log(getGrade(55))
+console.log(getGrade(55));
+
+function check_evening_class(day) {
+  switch (day) {
+    case "Monday":
+      return "You have no evening class today";
+
+    case "Tuesday":
+      return "You have intro to web development tonight";
+
+    case "Wednesday":
+      return "You have no evening class today";
+
+    case "Thursday":
+      return "You have intro to web development tonight";
+
+    case "Friday":
+      return "You have no evening class today";
+
+    default:
+      return "Its the weekend!";
+  }
+}
