4ff45948ca6c3db4e53eac6d021b838e96aa1c5f
Tlinton1 <tenishal@hotmail.co.uk>
2023-11-15 19:23:07 +0000

    done

--- session-2/index.js
+++ session-2/index.js
@@ -1,5 +1,10 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
-var a = 6
+var a = 6;
 
-console.log(a)
+console.log(a);
+
+document.getElementById("first_para").style.backgroundColor = "red";
+console.log(document.getElementById("first_para"));
+console.log(document.querySelector(".about").innerHTML);
+console.log(document.querySelectorAll("#about"));
